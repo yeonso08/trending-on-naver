15fe428a3a5d784afbfcc598be6082dce2f0b125
jaeang <hjng0825@gmail.com>
2025-01-07 14:32:41 +0900

    chore : shadcn 테마 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
